0f706b2c202e1d78cf59a4889b85b8fb38bc3a97
jmendozacr <mendoza077@gmail.com>
2019-09-16 21:32:26 -0600

    setup project and adding Question component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Question from './component/Question';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App container">
+      <header>
+        <h1>Weekly Budget</h1>
+
+        <div className="main-content content">
+          <Question/>
+        </div>
       </header>
     </div>
   );
---
+++ src/component/Question.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+
+export default function Question() {
+
+    const [amount, setAmount] = useState(0);
+    const [error, setError] = useState(false);
+
+    const addBudget = e => {
+        e.preventDefault();
+
+        if(amount < 1 || isNaN(amount)) {
+            setError(true);
+            return;
+        }
+    }
+
+
+    return(
+        <>
+            <h2>Place your budget</h2>
+            {
+                error ?
+                    <p className="alert alert-danger error">The budget is incorrect.</p>
+                    : null
+            }
+            <form onSubmit={addBudget}>
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Add your budget"
+                    onChange={e => setAmount( parseInt(e.target.value, 10) )}
+                />
+                <input
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Define Budget"
+                />
+            </form>
+        </>
+    );
+}
